90be61287e8069d7c315e1d7af4353bbfeb99b83
Vasudev Jaiswal <vasudevjaiswal050405@gmail.com>
2026-01-17 02:32:30 +0530

    feat: add OAuth

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,7 +1,7 @@
-import type { auth } from "@rntu-visit/auth";
 import { createAuthClient } from "better-auth/react";
-import { inferAdditionalFields } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-	plugins: [inferAdditionalFields<typeof auth>()],
+  baseURL: process.env.NEXT_PUBLIC_APP_URL,
 });
+
+export const { signIn, signOut, signUp, useSession } = authClient;
