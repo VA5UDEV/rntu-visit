1617347ee9b77a554a9c6558836e586f3afd47e7
Vasudev Jaiswal <vasudevjaiswal050405@gmail.com>
2026-01-17 02:33:16 +0530

    feat: update login page

--- apps/web/src/app/login/page.tsx
+++ apps/web/src/app/login/page.tsx
@@ -1,15 +1,9 @@
-"use client";
+import SignIn from "@/components/register";
 
-import SignInForm from "@/components/sign-in-form";
-import SignUpForm from "@/components/sign-up-form";
-import { useState } from "react";
-
-export default function LoginPage() {
-	const [showSignIn, setShowSignIn] = useState(false);
-
-	return showSignIn ? (
-		<SignInForm onSwitchToSignUp={() => setShowSignIn(false)} />
-	) : (
-		<SignUpForm onSwitchToSignIn={() => setShowSignIn(true)} />
-	);
+export default function SignInPage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-background px-4">
+      <SignIn />
+    </div>
+  );
 }
