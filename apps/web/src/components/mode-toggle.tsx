e1b001637894070c61f892cc2cfef80a9e964a5a
Vasudev Jaiswal <vasudevjaiswal050405@gmail.com>
2026-01-16 16:57:07 +0530

    feat: ui fix

--- apps/web/src/components/mode-toggle.tsx
+++ apps/web/src/components/mode-toggle.tsx
@@ -25,10 +25,10 @@ export function ModeToggle() {
 
   return (
     <Button
-      variant="outline"
+      variant="ghost"
       size="icon"
       onClick={toggleTheme}
-      className="rounded-full w-10 h-10 bg-transparent"
+      className="rounded-full w-16 h-16 bg-transparent"
     >
       <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
