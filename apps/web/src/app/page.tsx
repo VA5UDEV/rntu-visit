64ac727ea9b21b73f13c5187875eb18477b0ffd8
Vasudev Jaiswal <vasudevjaiswal050405@gmail.com>
2026-01-16 16:55:44 +0530

    bug: fix import

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,10 +1,10 @@
-import { LetsWorkTogether } from "@/components/hero";
+import { Hero } from "@/components/hero";
 
 export default function Home() {
 
 	return (
-		<div className="container mx-auto max-w-3xl px-4 py-2">
-      <LetsWorkTogether />
+		<div className="container mx-auto max-w-3xl px-4">
+      <Hero />
 		</div>
 	);
 }
