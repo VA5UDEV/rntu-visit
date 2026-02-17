3be5bffc61897d4bd9e3a29be0541656be66216b
Vasudev Jaiswal <vasudevjaiswal050405@gmail.com>
2026-02-18 05:22:07 +0530

    feat: fix

---
+++ apps/web/src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import SignIn from "@/components/register";
+
+export default function SignInPage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-background px-4">
+      <SignIn />
+    </div>
+  );
+}
