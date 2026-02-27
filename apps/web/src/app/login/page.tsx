9f5a909aa6e46202ddb06a163b99695937054c46
Vasudev Jaiswal <vasudevjaiswal050405@gmail.com>
2026-02-27 23:42:24 +0530

    feat: ui changes

--- apps/web/src/app/login/page.tsx
+++ apps/web/src/app/login/page.tsx
@@ -1,9 +1,15 @@
 import SignIn from "@/components/register";
+import { RntuLogo } from "@/components/rntu-logo";
 
 export default function SignInPage() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-background px-4">
-      <SignIn />
+    <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-4 p-6 md:p-10">
+      <a href="/" className="flex items-center self-center">
+        <RntuLogo />
+      </a>
+      <div className="w-full max-w-sm">
+        <SignIn />
+      </div>
     </div>
   );
 }
